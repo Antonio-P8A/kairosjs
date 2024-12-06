e47fce1f7476fc7256271c93b70ca71d6ee151e4
Antonio Peña <antonio.pena@maicrosoft.eu>
2024-12-06 21:42:22 +0100

    Prueba de push 1

---
+++ src/kairos.js
@@ -0,0 +1,8 @@
+/**
+* @version: 1.0.0
+* @author: Antonio Peña https://www.ajdev.es/
+* @copyright: Copyright (c) 2024 Antonio Peña. Todos los derechos reservados.
+* @license: Licensed under the MIT license. 
+* You may obtain a copy of the License at
+* https://opensource.org/licenses/MIT
+*/
